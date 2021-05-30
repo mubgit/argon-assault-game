def14a251baaefad4ff888860ebb64c41bdfaa5c
mu6ariz <z.mubariz25@gmail.com>
2021-05-30 03:59:25 -0700

    added reload level by if collide

--- Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-
-    void OnCollisionEnter(Collision collision)
-    {
-
-        Debug.Log(this.name + "collided with" + collision.gameObject.name);
-
-    }
+    [SerializeField] float loadDelay = 1f;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.name} **Triggered by** {other.gameObject.name}");
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
